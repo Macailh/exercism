ac3e01c37bff0d7bf8378a600e8997172f754b47
Macailh <salvadorenriquegb@gmail.com>
2023-04-07 12:52:14 -0600

    Add resistor color duo problem

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,6 @@
+COLORS = ['black', 'brown', 'red', 'orange', 'yellow',
+          'green', 'blue', 'violet', 'grey', 'white']
+
+
+def value(colors):
+    return int(''.join(str(COLORS.index(c)) for c in colors[:2]))
